4cdf9920e3ea43b0183df855994c2b2030d3b4ca
beebablaster <sanzharsyzdykov1738@gmail.com>
2023-10-24 17:43:30 +0600

    feat: mock server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
+import {Component, OnInit} from '@angular/core';
 import { Task } from '../../Task';
+import { TaskService } from "../../services/task.service";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {
+  }
+
+  ngOnInit() {
+    this.taskService.getTasks().subscribe((tasks) =>
+    this.tasks = tasks);
+  }
 }
